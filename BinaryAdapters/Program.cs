28dc3b8a383e77864bf5b29164e718434d187be4
LABORATORIO 3 <LABORATORIO 3@DESKTOP-NVKFFL7>
2024-03-20 08:33:52 -0600

    Agregar archivos de proyecto.

---
+++ BinaryAdapters/Program.cs
@@ -0,0 +1,15 @@
+﻿using BinaryAdapters;
+
+string filePath = "people.bin";
+
+IPersonRepository personRepository = new PersonBinaryRepository(filePath);
+
+
+//Agregar algunas personas 
+personRepository.Add(new Person { Name = "Juan", Age = 17, Height = 170});
+personRepository.Add(new Person { Name = "Maria", Age = 18, Height = 160});
+personRepository.Add(new Person { Name = "Carlos", Age = 20, Height = 182});
+
+Console.WriteLine($"Datos de personas guardados en{filePath}");
+foreach (Person person in personRepository.GetAll())
+    Console.WriteLine($"Id: {person.Id},Nombre : {person.Name},Edad{person.Age}, Altura {person.Height}");
---
+++ UsoAdaptadores/Program.cs
@@ -0,0 +1,26 @@
+﻿//Ejemplo de escritura y lectura de datosen un rchivo de texto
+
+string texto = "Ejemplo de texto.";
+File.WriteAllText("ejemplo.txt",texto);
+
+//Leer los datos
+string texto2 = File.ReadAllText("ejemplo.txt");
+Console.WriteLine($"Texto leído desde el archivo: {texto2}");
+
+//Escribir datos en un archivo binario
+int[] numeros = { 1, 2, 3, 4, 5 };
+using (BinaryWriter br = new BinaryWriter(File.Open("numeros.bin", FileMode.Create)))
+        foreach(int numero in numeros)
+             br.Write(numero);
+
+//Leer datos de un archivo binario
+int[] numerosLeidos;
+using(BinaryReader reader = new BinaryReader(File.Open("numeros.bin", FileMode.Open)))
+{
+    numerosLeidos = new int[5];
+    for(int i = 0; i < numerosLeidos.Length; i++)
+        numerosLeidos[i] = reader.ReadInt32();
+}
+Console.WriteLine("Numeros leidos desde un archivo binario");
+foreach(int numero in numerosLeidos)
+    Console.WriteLine(numero);
